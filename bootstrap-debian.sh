8bdbd74f2f91633dd16ae00f8db030a0b8482f88
Ronny Trommer <ronny@opennms.org>
2015-11-05 11:17:57 +0100

    Add variable for release file

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -13,7 +13,7 @@ MIRROR="debian.mirrors.opennms.org"
 ANSWER="No"
 
 REQUIRED_SYSTEMS="Ubuntu|Debian"
-SYSTEM=$(cat /etc/issue | grep -E ${REQUIRED_SYSTEMS})
+RELEASE_FILE="/etc/issue"
 
 # Error codes
 E_ILLEGAL_ARGS=126
@@ -72,7 +72,7 @@ showDisclaimer() {
 }
 
 # Test if system is supported
-cat /etc/issue | grep -E ${REQUIRED_SYSTEMS}  1>/dev/null 2>>${ERROR_LOG}
+cat ${RELEASE_FILE} | grep -E ${REQUIRED_SYSTEMS}  1>/dev/null 2>>${ERROR_LOG}
 if [ ! ${?} -eq 0 ]; then
   echo ""
   echo "This is system is not a supported Ubuntu or Debian system."
